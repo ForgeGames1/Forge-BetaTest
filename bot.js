77c9b8914af80b376e3f3f0b8dd642d356219a4d
ForgeGames1 <34386594+ForgeGames1@users.noreply.github.com>
2018-12-30 14:23:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,4 +24,4 @@ bot.on('message', async message => {
 });
 
 
-bot.login('NDA3MTgxODM4NjI5NDcwMjA4.DwpSXQ.F-32WnicHACQdrTYt5L6h-GS5iU');
+bot.login('NTI4OTA4MTI1NzU1OTMyNjcz.DwpYSg.Su3NLqHCP2923sYeqMIbf_nR0fQ');
